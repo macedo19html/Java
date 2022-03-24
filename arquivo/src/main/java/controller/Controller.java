11736e5e6da3b87a13678c985ff04b1a0869137f
João Macedo Macena <73135265+macedo19html@users.noreply.github.com>
2022-03-23 22:34:58 -0300

    Adicionei o projeto Arquivo

---
+++ arquivo/src/main/java/controller/Controller.java
@@ -0,0 +1,257 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import model.DAO;
+import model.JavaBeans;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Controller.
+ */
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert", "/select", "/update", "/delete", "/report" })
+public class Controller extends HttpServlet {
+	
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+	
+	/** The dao. */
+	DAO dao = new DAO();
+	
+	/** The contato. */
+	JavaBeans contato = new JavaBeans();
+
+	/**
+	 * Instantiates a new controller.
+	 */
+	public Controller() {
+		super();
+	}
+
+	/**
+	 * Do get.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		if (action.equals("/main")) {
+			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			adicionarContato(request, response);
+		} else if (action.equals("/select")) {
+			listarContato(request, response);
+		} else if (action.equals("/update")) {
+			editarContato(request, response);
+		} else if (action.equals("/delete")) {
+			removerContato(request, response);
+		} else if (action.equals("/report")) {
+			gerarRelatorio(request, response);
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+	/**
+	 * Contatos.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// Listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Criar um objeto que ira receber os dados JavaBeans
+		ArrayList<JavaBeans> lista = dao.listarContatos();
+		// Encaminhar a lista ao documento arquivo.jsp
+		request.setAttribute("contatos", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("arquivo.jsp");
+		rd.forward(request, response);
+		// teste de recebimento da lista
+		/*
+		 * for (int i = 0; i < lista.size(); i++) {
+		 * System.out.println(lista.get(i).getIdcon());
+		 * System.out.println(lista.get(i).getNome());
+		 * System.out.println(lista.get(i).getFone());
+		 * System.out.println(lista.get(i).getEmail());
+		 * 
+		 * }
+		 */
+	}
+
+	/**
+	 * Adicionar contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// Novo contato
+	protected void adicionarContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// teste de recebimento dos dados do formulario
+		// System.out.println(request.getParameter("nome"));
+		// System.out.println(request.getParameter("fone"));
+		// System.out.println(request.getParameter("email"));
+
+		// setar as variaveis JavaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+
+		// invocar o metodo inserirContato passando o objeto contato
+		dao.inserirContato(contato);
+		// redirecionar para o documento arquivo.jsp
+		response.sendRedirect("main");
+	}
+
+	/**
+	 * Listar contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// Editar contato
+	protected void listarContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// setar a variavel JavaBe ans
+		contato.setIdcon(request.getParameter("idcon"));
+		// Executar o metodo selecionarContato (DAO)
+		dao.selecionarContato(contato);
+
+		// Setar os atributos do formulario com o conteudo javabeans
+		request.setAttribute("idcon", contato.getIdcon());
+		request.setAttribute("nome", contato.getNome());
+		request.setAttribute("fone", contato.getFone());
+		request.setAttribute("email", contato.getEmail());
+
+		// Encaminhar ao documento editar.jsp
+		RequestDispatcher rd = request.getRequestDispatcher("editar.jsp");
+		rd.forward(request, response);
+
+		/*
+		 * teste de recebimento System.out.println(contato.getIdcon());
+		 * System.out.println(contato.getNome()); System.out.println(contato.getFone());
+		 * System.out.println(contato.getEmail());
+		 */
+	}
+
+	/**
+	 * Editar contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	protected void editarContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// teste de recebimento
+		/// System.out.println(request.getParameter("idcon"));
+		// System.out.println(request.getParameter("nome"));
+		// System.out.println(request.getParameter("fone"));
+		// System.out.println(request.getParameter("email"));
+
+		// setar as variaveis Javabeans
+		contato.setIdcon(request.getParameter("idcon"));
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		// executar o metodo alterarContato
+		dao.alterarContato(contato);
+
+		// redirecionar para o documento arquivo.jsp (atualizando as alterações
+		response.sendRedirect("main");
+	}
+
+	/**
+	 * Remover contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// Remover um contato
+	protected void removerContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// setar a variavel idcon JavaBeans
+		contato.setIdcon(request.getParameter("idcon"));
+		// executar o metodo deletarContato (DAO) passando o objeto contato
+		dao.deletarContato(contato);
+		// redirecionar para o documento arquivo.jsp (atualizando as alterações
+		response.sendRedirect("main");
+	}
+
+	/**
+	 * Gerar relatorio.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// Gerar relatorio em PDF
+	protected void gerarRelatorio(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Document documento = new Document();
+		try {
+			// tipo de conteúdo
+			response.setContentType("apllication/pdf");
+			// nome do documento
+			response.addHeader("Content-Disposition", "inline; filename=" + "contatos.pdf");
+			// Criar o documento
+			PdfWriter.getInstance(documento, response.getOutputStream());
+			// abrir o documento -> coteúdo
+			documento.open();
+			documento.add(new Paragraph("Lista de contatos:"));
+			documento.add(new Paragraph(" "));
+			// criar uma tabala
+			PdfPTable tabela = new PdfPTable(3);
+			// cabeçalho
+			PdfPCell col1 = new PdfPCell(new Paragraph("Nome"));
+			PdfPCell col2 = new PdfPCell(new Paragraph("Fone"));
+			PdfPCell col3 = new PdfPCell(new Paragraph("E-mail"));
+			tabela.addCell(col1);
+			tabela.addCell(col2);
+			tabela.addCell(col3);
+			// popular a tabela com os contatos
+			ArrayList<JavaBeans> lista = dao.listarContatos();
+			for (int i = 0; i < lista.size(); i++) {
+				tabela.addCell(lista.get(i).getNome());
+				tabela.addCell(lista.get(i).getFone());
+				tabela.addCell(lista.get(i).getEmail());
+			}
+			documento.add(tabela);
+			documento.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			documento.close();
+		}
+	}
+}
---
+++ arquivo/src/main/java/model/DAO.java
@@ -0,0 +1,180 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class DAO.
+ */
+public class DAO {
+	
+	/**  Modulo de conxao *. */
+	// Parametros de conexao
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	
+	/** The url. */
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbarquivo?useTimezone=true&serverTimezone=UTC";
+	
+	/** The user. */
+	private String user = "root";
+	
+	/** The password. */
+	private String password = "macedo19";
+
+	/**
+	 * Conectar.
+	 *
+	 * @return the connection
+	 */
+	// metodo de conexao
+	private Connection conectar() {
+		Connection con = null;
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	/**
+	 *  CRUD CREATE *.
+	 *
+	 * @param contato the contato
+	 */
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email) values (?,?,?)";
+
+		try {
+			// abrindo a conexao
+			Connection con = conectar();
+
+			// Preparar a query para execuçao no banco de dados
+			PreparedStatement pst = con.prepareStatement(create);
+
+			// Substituir os parametros (?) pelo conteudo das variaveis JavaBeans
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+
+			// Executar a query
+			pst.executeUpdate();
+			// Encerrar a conexao com o banco
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 *  CRUD READ *.
+	 *
+	 * @return the array list
+	 */
+	public ArrayList<JavaBeans> listarContatos() {
+		// Criando um objeto para acessar a classe JavaBeans
+		ArrayList<JavaBeans> contatos = new ArrayList<>();
+		String read = "select * from contatos order by nome";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst = con.prepareStatement(read);
+			ResultSet rs = pst.executeQuery();
+
+			// o laço abaixo sera executado enquanto houver contatos
+			while (rs.next()) {
+				// variaveis de apoio que recebem os dados de bancos de dados
+				String idcon = rs.getString(1);
+				String nome = rs.getString(2);
+				String fone = rs.getString(3);
+				String email = rs.getString(4);
+				// populando o ArrayList
+				contatos.add(new JavaBeans(idcon, nome, fone, email));
+			}
+			con.close();
+			return contatos;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	/**
+	 *  CRUD UPDATE *.
+	 *
+	 * @param contato the contato
+	 */
+	public void selecionarContato(JavaBeans contato) {
+		String read2 = "select * from contatos where idcon = ?";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst =con.prepareStatement(read2);
+			pst.setString(1, contato.getIdcon());
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				// setar as variaveis Javabeans
+				contato.setIdcon(rs.getString(1));
+				contato.setNome(rs.getString(2));
+				contato.setFone(rs.getString(3));
+				contato.setEmail(rs.getString(4));
+			}
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 * Alterar contato.
+	 *
+	 * @param contato the contato
+	 */
+	// editar o contato
+	public void alterarContato(JavaBeans contato) {
+		String update = "update contatos set nome=?,fone=?,email=? where idcon=?";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst = con.prepareStatement(update);
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			pst.setString(4, contato.getIdcon());
+			pst.executeUpdate();
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 *  CRUD DELETE *.
+	 *
+	 * @param contato the contato
+	 */
+	public void deletarContato(JavaBeans contato) {
+		String delete = "delete from contatos where idcon=?";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst = con.prepareStatement(delete);
+			pst.setString(1, contato.getIdcon());
+			pst.executeUpdate();
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	// teste de conexao
+	/*
+	 * public void testeConexao() { try { Connection con = conectar();
+	 * System.out.println(con); con.close(); } catch (Exception e) { // TODO: handle
+	 * exception } }
+	 */
+
+}
